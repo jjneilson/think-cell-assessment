a030a709e9fd7e94bdd74006e574d38442172e05
neils <neilson.john38781@gmail.com>
2023-11-04 13:20:02 -0400

    Final Submission fails big-O optimization

--- solution.cpp
+++ solution.cpp
@@ -1,57 +1,123 @@
 #include <map>
 
-template<typename K, typename V>
+template <typename K, typename V>
 
-class interval_map {
-	friend void IntervalMapTest();
-	V m_valBegin;
-	std::map<K,V> m_map;
+class interval_map
+{
+    friend void IntervalMapTest();
+    V m_valBegin;
+    std::map<K, V> m_map;
 
 public:
-	// constructor associates whole range of K with val
-	interval_map(V const& val)
-	: m_valBegin(val)
-	{}
+    // constructor associates whole range of K with val
+    interval_map(V const &val)
+        : m_valBegin(val)
+    {
+    }
 
-	// Assign value val to interval [keyBegin, keyEnd).
-	// Overwrite previous values in this interval.
-	// Conforming to the C++ Standard Library conventions, the interval
-	// includes keyBegin, but excludes keyEnd.
-	// If !( keyBegin < keyEnd ), this designates an empty interval,
-	// and assign must do nothing.
-	void assign( K const& keyBegin, K const& keyEnd, V const& val ) {
+    // Assign value val to interval [keyBegin, keyEnd).
+    // Overwrite previous values in this interval.
+    // Conforming to the C++ Standard Library conventions, the interval
+    // includes keyBegin, but excludes keyEnd.
+    // If !( keyBegin < keyEnd ), this designates an empty interval,
+    // and assign must do nothing.
+    void assign(K const &keyBegin, K const &keyEnd, V const &val)
+    {
         if (!(keyBegin < keyEnd))
             return;
 
-	    // Find the insertion points
-	    auto itBegin = m_map.upper_bound(keyBegin);
-	    auto itEnd = m_map.upper_bound(keyEnd);
+        if (m_map.empty())
+        {
+            if (val == m_valBegin)
+                return;
 
-	    if (itBegin == itEnd)
-	    {
-		    m_map.insert(itBegin, std::make_pair(keyBegin,val));
-		    m_map.insert(itEnd, std::make_pair(keyEnd, m_valBegin));
-	    }
+            m_map.insert(std::pair<K, V>(keyBegin, val));
+            m_map.insert(std::pair<K, V>(keyEnd, m_valBegin));
 
-	    // Update the value at the begin key
-	    if (itBegin == m_map.end() || itBegin->second != val)
-		    m_map.insert_or_assign(itBegin, keyBegin, val);
+            return;
+        }
 
-	    // Remove any entries within the range (excluding the end key)
-	    m_map.erase(std::next(itBegin), itEnd);
+        auto itLowerBound = m_map.upper_bound(keyBegin);
+        auto itLowerBoundExactSame = itLowerBound;
+        if (itLowerBoundExactSame != m_map.begin())
+        {
+            itLowerBoundExactSame = std::prev(itLowerBoundExactSame);
+            if (!(itLowerBoundExactSame->first < keyBegin))
+                itLowerBound = itLowerBoundExactSame;
+        }
 
-	    // Insert the end key with the default value if it doesn't already exist
-	    if (itEnd == m_map.end() || itEnd->second != m_valBegin)
-		    m_map.insert(itEnd, { keyEnd, m_valBegin });
+        auto itHigherBound = m_map.upper_bound(keyEnd);
+
+        if (itLowerBound == m_map.end() || itHigherBound == m_map.begin())
+        {
+            if (val == m_valBegin)
+                return;
+
+            m_map.insert(std::pair<K, V>(keyBegin, val));
+            m_map.insert(std::pair<K, V>(keyEnd, m_valBegin));
+
+            return;
+        }
+
+        auto itFindBeginValue = itLowerBound;
+
+        V beginValue(m_valBegin);
+        if (itFindBeginValue != m_map.begin())
+            beginValue = std::prev(itFindBeginValue)->second;
+
+        auto itFindEndValue = itHigherBound;
+
+        V endValue(m_valBegin);
+        if (itFindEndValue != m_map.end())
+            endValue = std::prev(itFindEndValue)->second;
+
+        if (itLowerBound != m_map.end() && itHigherBound != m_map.end())
+        {
+            if (std::distance(itLowerBound, itHigherBound) == 0)
+            {
+                if (val == beginValue)
+                    return;
+
+                m_map.insert(std::pair<K, V>(keyBegin, val));
+                m_map.insert(std::pair<K, V>(keyEnd, beginValue));
+
+                return;
+            }
+        }
+        m_map.erase(itLowerBound, itHigherBound);
+
+        if (m_map.empty())
+        {
+            if (val == m_valBegin)
+                return;
+
+            m_map.insert(std::pair<K, V>(keyBegin, val));
+            m_map.insert(std::pair<K, V>(keyEnd, m_valBegin));
+
+            return;
+        }
+
+        if (!(beginValue == val))
+            m_map.insert(std::pair<K, V>(keyBegin, val));
+
+        if (!(endValue == val))
+            m_map.insert(std::pair<K, V>(keyEnd, endValue));
+
+        return;
+
     }
 
-	// look-up of the value associated with key
-	V const& operator[]( K const& key ) const {
-		auto it=m_map.upper_bound(key);
-		if(it==m_map.begin()) {
-			return m_valBegin;
-		} else {
-			return (--it)->second;
-		}
-	}
+    // look-up of the value associated with key
+    V const &operator[](K const &key) const
+    {
+        auto it = m_map.upper_bound(key);
+        if (it == m_map.begin())
+        {
+            return m_valBegin;
+        }
+        else
+        {
+            return (--it)->second;
+        }
+    }
 };
